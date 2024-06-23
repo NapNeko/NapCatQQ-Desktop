8dbc89eeb0f92e14d7887559df10a5ac65b7085b
Qiao <572381217@qq.com>
2024-06-23 09:15:55 +0800

    feat: Add single_shot functionality to timer decorator
    - Enhance timer decorator with single_shot option to create QTimer that times out only once.
    - Allow more flexible usage of QTimer within decorated functions by enabling one-time timeout.

--- src/Core/__init__.py
+++ src/Core/__init__.py
@@ -1,22 +1,18 @@
 # -*- coding: utf-8 -*-
-import sys
 from functools import wraps
 from typing import Callable, Any
 
 from PySide6.QtCore import QTimer, QObject
-from PySide6.QtWidgets import QApplication
 
 
-def timer(interval: int) -> Callable[[Callable[..., Any]], Callable[..., Any]]:
+def timer(interval: int, single_shot: bool = False) -> Callable[[Callable[..., Any]], Callable[..., Any]]:
     """
-    将 QTimer 附加到它所修饰的类方法的实例的修饰器。
-    计时器以“interval”指定的定期间隔调用修饰函数。
+    ## 将 QTimer 附加到它所修饰的类方法的实例的修饰器
+    ## 计时器以 “interval” 指定的定期间隔调用修饰函数
 
-    Args:
-        interval （int）：计时器应触发的间隔（以毫秒为单位）。
+        - interval （int）: 计时器应触发的间隔（以毫秒为单位）
+        - single_shot （bool）: 如果为 True，则计时器只超时一次, 默认值为 False
 
-    Returns:
-        Callable[[Callable[...， Any]]， Callable[...， Any]]：修饰函数。
     """
 
     def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
@@ -24,29 +20,17 @@ def timer(interval: int) -> Callable[[Callable[..., Any]], Callable[..., Any]]:
         def wrapper(*args: Any, **kwargs: Any) -> Any:
             instance = args[0]
             if not hasattr(instance, '_timers'):
-                instance.timers = []
+                instance._timers = []
 
             timer_instance = QTimer(instance)
+            timer_instance.setInterval(interval)
+            timer_instance.setSingleShot(single_shot)
             timer_instance.timeout.connect(lambda: func(*args, **kwargs))
-            timer_instance.start(interval)
+            timer_instance.start()
 
-            instance.timers.append(timer_instance)  # 将计时器保存到实例变量
+            instance._timers.append(timer_instance)  # 将计时器保存到实例变量
             return func(*args, **kwargs)
 
         return wrapper
 
     return decorator
-
-
-# Example usage
-if __name__ == "__main__":
-    class ExampleClass(QObject):
-        @timer(1000)
-        def print_message(self):
-            print("Hello, world!")
-    app = QApplication(sys.argv)
-
-    example = ExampleClass()
-    example.print_message()
-
-    sys.exit(app.exec_())
