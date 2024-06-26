2a8947d6efb5853894bf62a2794132046bcd9d7a
Qiao <572381217@qq.com>
2024-06-26 21:46:50 +0800

    fix

--- src/Core/BeginnerGuidance.py
+++ src/Core/BeginnerGuidance.py
@@ -1,1 +1,34 @@
 # -*- coding: utf-8 -*-
+from PySide6.QtCore import QObject
+from creart import it
+
+from src.Ui.MainWindow.Window import MainWindow
+
+
+class BeginnerGuidance(QObject):
+    """
+    ## 新手引导(首次启动时)
+        - 引导用户安装 NapCat, QQ
+    """
+
+    def __init__(self, parent):
+        super().__init__()
+        # 禁用所有侧边栏
+        it(MainWindow).home_widget_button.setEnabled(False)
+        it(MainWindow).add_widget_button.setEnabled(False)
+        it(MainWindow).bot_list_widget_button.setEnabled(False)
+        it(MainWindow).setup_widget_button.setEnabled(False)
+
+    def skip(self):
+        """
+        ## 询问是否要跳过引导
+        """
+
+
+    def toDownloadPage(self):
+        """
+        ## 跳转到 Download 页面并指引下载
+        """
+
+
+
