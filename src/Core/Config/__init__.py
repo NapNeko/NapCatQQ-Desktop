331ac229b26847c79ba5ec00f7359954487924c3
Qiao <572381217@qq.com>
2024-09-29 19:15:52 +0800

    🌟 releases: 发布 v1.0.1 版本

--- src/Core/Config/__init__.py
+++ src/Core/Config/__init__.py
@@ -124,6 +124,6 @@ class Config(QConfig):
 cfg = Config()
 qconfig.load(it(PathFunc).config_path, cfg)
 cfg.set(cfg.StartTime, time.time(), True)
-cfg.set(cfg.NCDVersion, "v1.0.0", True)
+cfg.set(cfg.NCDVersion, "v1.0.1", True)
 cfg.set(cfg.SystemType, platform.system(), True)
 cfg.set(cfg.PlatformType, platform.machine(), True)
