3a0aa7e816ca2d198221c82c5b2e0dec3736f2de
Qiao <SugarQiao@outlook.com>
2025-02-15 13:01:17 +0800

    ♻️ refactor(Core): 调整Path功能

--- src/Core/Utils/path.py
+++ src/Core/Utils/path.py
@@ -11,45 +11,31 @@ from src.core.utils.singleton import Singleton
 class PathFunc(metaclass=Singleton):
 
     def __init__(self):
-        """
-        ## 初始化
-        """
 
-        # 路径字段
+        # QQ路径(未定义,从注册表中获取)
         self.qq_path = None
-        self.base_path = Path.cwd()
+
+        # NCD运行所用到的路径
+        self.base_path = Path.cwd() / "NCD Files"
         self.napcat_path = self.base_path / "NapCat"
         self.config_dir_path = self.base_path / "config"
         self.tmp_path = self.base_path / "tmp"
 
-        # 文件字段
-        self.config_path = self.config_dir_path / "config.json"
-        self.bot_config_path = self.config_dir_path / "bot.json"
-
     def path_validator(self) -> None:
-        """
-        ## 验证一系列路径
-        """
-
-        paths_to_validate = [(self.tmp_path, "Tmp"), (self.config_dir_path, "Config"), (self.napcat_path, "NapCat")]
-
-        for path, name in paths_to_validate:
+        """验证路径是否存在"""
+        for path in [path for path in self.__dict__.values() if isinstance(path, Path)]:
+            # 遍历属性提取出 Path 进行验证
             if not path.exists():
                 path.mkdir(parents=True, exist_ok=True)
-                logger.info(f"创建路径 {name.center(8)} 成功", LogType.FILE_FUNC, LogSource.CORE)
+                logger.info(f"创建路径{path.name.center(12)}成功", LogType.FILE_FUNC, LogSource.CORE)
             else:
-                logger.info(f"路径 {name.center(8)} 已存在", LogType.FILE_FUNC, LogSource.CORE)
+                logger.info(f"路径{path.name.center(12)}已存在", LogType.FILE_FUNC, LogSource.CORE)
 
-    @staticmethod
-    def get_qq_path() -> Path | None:
-        """
-        获取QQ路径
-        """
+    def get_qq_path(self) -> Path | None:
+        """读取注册表获取QQ路径"""
         try:
-            key = winreg.OpenKey(
-                key=winreg.HKEY_LOCAL_MACHINE,
-                sub_key=r"SOFTWARE\WOW6432Node\Tencent\QQNT",
-            )
-            return Path(winreg.QueryValueEx(key, "Install")[0])
+            key = winreg.OpenKey(key=winreg.HKEY_LOCAL_MACHINE, sub_key=r"SOFTWARE\WOW6432Node\Tencent\QQNT")
+            self.qq_path = Path(winreg.QueryValueEx(key, "InstallPath")[0])
+            return self.qq_path
         except FileNotFoundError:
             return None
