8c1509f207e3fdfab35835d3afa383e6a243c2fe
Qiao <SugarQiao@outlook.com>
2025-02-15 14:11:58 +0800

    ✨ feat(UI): 添加不需要适配主题切换的图标类

--- src/Ui/Icon.py
+++ src/Ui/Icon.py
@@ -6,11 +6,17 @@ from enum import Enum
 from qfluentwidgets.common import Theme, FluentIconBase, getIconColor
 
 
-class NapCatDesktopIcon(FluentIconBase, Enum):
-    """主窗体所需要的图标"""
-
-    LOGO = "Logo"
-    QQ = "QQ"
+class NCDFluentIcon(FluentIconBase, Enum):
+    """主窗体所需要的图标(适配主题切换)"""
 
     def path(self, theme=Theme.AUTO) -> str:
         return f":Icon/image/Icon/{getIconColor(theme)}/{self.value}.svg"
+
+
+class NCDIcon(Enum):
+    """主窗体所需要的图标(不适配主题切换)"""
+
+    LOGO = ":Global/logo.png"
+
+    def __str__(self) -> str:
+        return self.value
--- src/Ui/main_window/window.py
+++ src/Ui/main_window/window.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
-# 标准库导入
-from typing import Optional
-
 # 第三方库导入
-from qfluentwidgets import Theme, SplashScreen, MSFluentWindow
-from PySide6.QtGui import QIcon
+from qfluentwidgets import SplashScreen, MSFluentWindow
+from PySide6.QtGui import QPixmap
 from PySide6.QtCore import QSize
 from PySide6.QtWidgets import QApplication
 
 # 项目内模块导入
-from src.ui.icon import NapCatDesktopIcon
+from src.ui.icon import NCDIcon
 from src.ui.resource import resource
 from src.core.utils.path import PathFunc
 from src.core.utils.singleton import singleton
@@ -17,11 +14,7 @@ from src.core.utils.singleton import singleton
 
 @singleton
 class MainWindow(MSFluentWindow):
-    """
-    ## 程序的主窗体
-    """
-
-    splashScreen: Optional[SplashScreen]
+    """NapCat Desktop 的主窗体"""
 
     def __init__(self) -> None:
         """构造函数"""
@@ -39,14 +32,11 @@ class MainWindow(MSFluentWindow):
         设置窗体
         """
         # 标题栏部分
-        self.setWindowIcon(QIcon(NapCatDesktopIcon.LOGO.path(Theme.LIGHT)))
+        self.setWindowIcon(QPixmap(NCDIcon.LOGO.value))
         # 窗体大小以及设置打开时居中
-        self.setMinimumSize(1148, 720)
-        # desktop = QApplication.screens()[0].availableGeometry()
-        # width, height = desktop.width(), desktop.height()
-        # self.move(width // 2 - self.width() // 2, height // 2 - self.height() // 2)
+        self.setMinimumSize(1280, 720)
         # 创建 Splash Screen
-        self.splashScreen = SplashScreen(":Global/image/Global/logo.png", self, True)
+        self.splashScreen = SplashScreen(NCDIcon.LOGO.value, self, True)
         self.splashScreen.setIconSize(QSize(360, 260))
         self.splashScreen.raise_()
         # 显示窗体
@@ -55,7 +45,5 @@ class MainWindow(MSFluentWindow):
         QApplication.processEvents()
 
     def setItem(self) -> None:
-        """
-        设置侧边栏
-        """
+        """设置侧边栏"""
         # 添加子页面
