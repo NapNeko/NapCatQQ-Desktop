b8e9c5caa2173be0775be9d8e834b111aaabafcd
Qiao <SugarQiao@outlook.com>
2025-02-16 11:04:50 +0800

    ✨ feat(Core): 添加信号传输

---
+++ src/Ui/common/signal_bus.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+NCD 信号总线
+"""
+from PySide6.QtCore import Signal, QObject
+
+
+class SignalBus(QObject):
+    """信号总线"""
+
+    # 不知道后续要不要做扩展
+
+    # 云母切换信号
+    micaEnableChanged = Signal(bool)
+
+
+signalBus = SignalBus()
