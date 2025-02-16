0f53afc6e89931a63012fc8a0597986b61ca332c
Qiao <SugarQiao@outlook.com>
2025-02-16 11:02:08 +0800

    🔧 chore(Utils): 更新路径验证逻辑

--- src/Core/Utils/path.py
+++ src/Core/Utils/path.py
@@ -16,15 +16,21 @@ class PathFunc(metaclass=Singleton):
         self.qq_path = None
 
         # NCD运行所用到的路径
-        self.base_path = Path.cwd() / "NCD Files"
+        self.base_path = Path.cwd() / ".NapCat Desktop"
         self.napcat_path = self.base_path / "NapCat"
         self.config_dir_path = self.base_path / "config"
         self.tmp_path = self.base_path / "tmp"
+
+        # 文件路径
+        self.config_path = self.config_dir_path / "config.json"
 
     def path_validator(self) -> None:
         """验证路径是否存在"""
         for path in [path for path in self.__dict__.values() if isinstance(path, Path)]:
             # 遍历属性提取出 Path 进行验证
+            if path.suffix:
+                # 此时有文件后缀,认为是文件,则跳过
+                continue
             if not path.exists():
                 path.mkdir(parents=True, exist_ok=True)
                 logger.info(f"创建路径{path.name.center(12)}成功", LogType.FILE_FUNC, LogSource.CORE)
