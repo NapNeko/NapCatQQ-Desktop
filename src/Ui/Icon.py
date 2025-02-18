88da7bf2d507824f6d25f0ac58a6018d7fb6e3a0
Qiao <SugarQiao@outlook.com>
2025-02-18 11:26:48 +0800

    ♻️ refactor(UI): 调整资源

--- src/Ui/Icon.py
+++ src/Ui/Icon.py
@@ -12,15 +12,17 @@ from src.core.config import cfg
 class NCDFluentIcon(FluentIconBase, Enum):
     """主窗体所需要的图标(适配主题切换)"""
 
+    APP_TITLE = "AppTitle"
+
     def path(self, theme=Theme.AUTO) -> str:
         theme = cfg.theme if theme == Theme.AUTO else theme
-        return f":Icon/icon/{theme.value.lower}/{self.value}.svg"
+        return f":Fluent-Icon/icons/{theme.value.lower()}/{self.value}.svg"
 
 
-class NCDIcon(Enum):
+class NCDIcon(FluentIconBase, Enum):
     """主窗体所需要的图标(不适配主题切换)"""
 
-    LOGO = ":Global/logo.png"
+    LOGO = ":Icon/icons/logo.png"
 
-    def __str__(self) -> str:
+    def path(self, theme=Theme.AUTO) -> str:
         return self.value
