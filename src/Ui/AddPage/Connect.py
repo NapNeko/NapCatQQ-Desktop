07c7d94d7982ebee8c15210b69c0adc1246b74b4
Qiao <SugarQiao@outlook.com>
2025-04-23 23:37:53 +0800

    ✨ feat(UI): 为Connect页面添加Defualt Page

--- src/Ui/AddPage/Connect.py
+++ src/Ui/AddPage/Connect.py
@@ -1,65 +1,46 @@
 # -*- coding: utf-8 -*-
+# 标准库导入
+import random
 
 # 第三方库导入
-from qfluentwidgets import ScrollArea, ExpandLayout
+from qfluentwidgets import BodyLabel, ImageLabel
+from PySide6.QtGui import QMovie
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QStackedWidget
 
 # 项目内模块导入
 from src.Core.Config.ConfigModel import ConnectConfig
 
 
-class ConnectWidget(ScrollArea):
+class ConnectWidget(QStackedWidget):
     """
-    ## Connect Item 项对应的 QWidget
+    ## 连接配置项对应的 QStackedWidget
     """
 
-    def __init__(self, parent=None, config: ConnectConfig = None) -> None:
+    def __init__(self, parent=None, config: ConnectConfig | None = None) -> None:
         super().__init__(parent=parent)
         self.setObjectName("ConnectWidget")
-        self.view = QWidget()
-        self.cardLayout = ExpandLayout(self)
-
-        # 设置 ScrollArea
-        self.setWidget(self.view)
-        self.setWidgetResizable(True)
-        self.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
-        self.view.setObjectName("ConnectWidgetView")
-
-        # 调用方法
-        self._initWidget()
-        self._setLayout()
 
         if config is not None:
             # 如果传入了 config 则进行解析并填充内部卡片
             self.config = config
             self.fillValue()
 
-    def _initWidget(self) -> None:
-        """
-        ## 初始化 QWidget 所需要的控件并配置
-        创建 InputCard
-        """
+        self._postInit()
 
-        self.cards = []
+    def _postInit(self) -> None:
+        """初始化"""
+        self.defultPage = DefaultPage(self)
+
+        self.addWidget(self.defultPage)
+
+        self.setCurrentWidget(self.defultPage)
 
     def fillValue(self) -> None:
         """
         ## 如果传入了 config 则对其内部卡片的值进行填充
         """
         ...
-
-    def _setLayout(self) -> None:
-        """
-        ## 将 QWidget 内部的 InputCard 添加到布局中
-        """
-        self.cardLayout.setContentsMargins(0, 0, 0, 0)
-        self.cardLayout.setSpacing(2)
-        for card in self.cards:
-            self.cardLayout.addWidget(card)
-            self.adjustSize()
-
-        self.view.setLayout(self.cardLayout)
 
     def getValue(self) -> dict:
         """
@@ -71,9 +52,38 @@ class ConnectWidget(ScrollArea):
         """
         ## 清空(还原)内部卡片的配置
         """
-        for card in self.cards:
-            card.clear()
+        ...
 
-    def adjustSize(self) -> None:
-        h = self.cardLayout.heightForWidth(self.width()) + 46
-        return self.resize(self.width(), h)
+
+class DefaultPage(QWidget):
+    """无配置时的默认页面"""
+
+    def __init__(self, parent: QWidget | None = None) -> None:
+        super().__init__(parent)
+
+        self.images = [
+            ":/FuFuFace/image/FuFuFace/g_fufu_01.gif",
+            ":/FuFuFace/image/FuFuFace/g_fufu_02.gif",
+            ":/FuFuFace/image/FuFuFace/g_fufu_03.gif",
+            ":/FuFuFace/image/FuFuFace/g_fufu_04.gif",
+            ":/FuFuFace/image/FuFuFace/g_fufu_05.gif",
+            ":/FuFuFace/image/FuFuFace/g_fufu_06.gif",
+            ":/FuFuFace/image/FuFuFace/g_fufu_07.gif",
+            ":/FuFuFace/image/FuFuFace/g_fufu_08.gif",
+            ":/FuFuFace/image/FuFuFace/g_fufu_09.gif",
+            ":/FuFuFace/image/FuFuFace/g_fufu_10.gif",
+        ]
+
+        self.imageLabel = ImageLabel(self)
+        self.movie = QMovie(random.choice(self.images))
+        self.label = BodyLabel(self.tr("还没有添加配置项喔"), self)
+        self.hBoxLayout = QVBoxLayout(self)
+
+        self.imageLabel.setMovie(self.movie)
+        self.imageLabel.scaledToWidth(self.width())
+
+        self.hBoxLayout.addStretch(1)
+        self.hBoxLayout.addWidget(self.imageLabel, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.hBoxLayout.addSpacing(12)
+        self.hBoxLayout.addWidget(self.label, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.hBoxLayout.addStretch(1)
