708e913c3c11de26c360838bc77c57de4347296c
Qiao <SugarQiao@outlook.com>
2025-05-02 13:00:58 +0800

    ✨ feat(UI): 调整窗体

--- src/Ui/GuideWindow/guide_window.py
+++ src/Ui/GuideWindow/guide_window.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 # 第三方库导入
 from qframelesswindow import FramelessWindow
-from PySide6.QtWidgets import QApplication
+from qfluentwidgets.components.widgets.stacked_widget import FadeEffectAniStackedWidget
+from PySide6.QtWidgets import QVBoxLayout, QApplication
 
 # 项目内模块导入
 from src.Core.Utils.singleton import singleton
+from src.Ui.GuideWindow.ask_page import AskPage
+from src.Ui.GuideWindow.welcome_page import WelcomePage
 
 
 @singleton
@@ -28,4 +31,39 @@ class GuideWindow(FramelessWindow):
         self.titleBar.minBtn.hide()
         self.titleBar.closeBtn.hide()
 
-    def create_page(self) -> None: ...
+        # 调用方法
+        self.create_page()
+        self.create_layout()
+
+    def create_page(self) -> None:
+        """创建页面等"""
+        self.view = FadeEffectAniStackedWidget(self)
+        self.welcomePage = WelcomePage(self)
+        self.askPage = AskPage(self)
+
+        self.view.addWidget(self.welcomePage)
+        self.view.addWidget(self.askPage)
+
+        self.view.setCurrentWidget(self.welcomePage)
+
+    def create_layout(self) -> None:
+        """创建布局"""
+        self.vBoxLayout = QVBoxLayout()
+        self.vBoxLayout.setContentsMargins(0, 0, 0, 0)
+        self.vBoxLayout.setSpacing(0)
+        self.vBoxLayout.addWidget(self.view)
+        self.setLayout(self.vBoxLayout)
+
+    def on_next_page(self) -> None:
+        """下一页"""
+        if isinstance(self.view.currentWidget(), WelcomePage):
+            self.view.setCurrentWidget(self.askPage)
+        elif isinstance(self.view.currentWidget(), AskPage):
+            self.view.setCurrentWidget(self.welcomePage)
+
+    def on_back_page(self) -> None:
+        """上一页"""
+        if isinstance(self.view.currentWidget(), WelcomePage):
+            self.view.setCurrentWidget(self.askPage)
+        elif isinstance(self.view.currentWidget(), AskPage):
+            self.view.setCurrentWidget(self.welcomePage)
