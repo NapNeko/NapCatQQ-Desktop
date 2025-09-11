c4aebb5f6ae9ccd941bae064bb59e3faae4cf736
Qiao <SugarQiao@outlook.com>
2025-09-11 08:09:16 +0800

    🐞 fix(UI): 修复使用信号总线导致反复触发的问题
     - 将信号总线改为一对一
    
    Severity: ⚠️ High
    Impact: 💥 Breaking
    
    小问题

--- src/ui/page/bot_list_page/bot_widget/meg_box.py
+++ src/ui/page/bot_list_page/bot_widget/meg_box.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from PySide6.QtCore import QObject, Slot
+from PySide6.QtCore import QObject, Signal
 
 # 项目内模块导入
 from src.ui.page.add_page.enum import ConnectType
@@ -9,13 +9,14 @@ from src.ui.page.bot_list_page.signal_bus import bot_list_page_signal_bus
 
 class ChooseConfigTypeDialog(OldChooseConfigTypeDialog):
 
-    def __init__(self, parent: QObject) -> None:
+    def __init__(self, parent: QObject, signal: Signal) -> None:
         super().__init__(parent=parent)
+        self.signal = signal
 
     def accept(self) -> None:
         """重写 accept 方法, 选择配置类型后发送信号"""
 
         if (id := self.button_group.checkedId()) != -1:
-            bot_list_page_signal_bus.choose_connect_type_signal.emit(list(ConnectType)[id])
+            self.signal.emit(list(ConnectType)[id])
 
         self.close()
--- src/ui/page/bot_list_page/signal_bus.py
+++ src/ui/page/bot_list_page/signal_bus.py
@@ -11,7 +11,6 @@ class BotListPageSignalBus(QObject):
 
     add_widget_view_change_signal = Signal(int)  # 添加页面视图切换信号, 带切换的索引
     add_connect_config_button_signal = Signal()  # 添加网络配置按钮点击信号
-    choose_connect_type_signal = Signal(Enum)  # 选择连接类型信号, 带连接类型的字符串
     remove_card_signal = Signal(QObject)  # 删除卡片信号, 带卡片
 
 
