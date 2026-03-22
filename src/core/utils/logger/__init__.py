303b9fdc0ec82ee220079edf7231ecb08989233d
Qiao <SugarQiao@outlook.com>
2026-03-22 11:42:08 +0800

    [refactor] 删除旧工具模块
    - 删除 src/core/utils/ 整个目录
    
    - 功能已迁移至新的模块架构

--- src/core/utils/logger/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# 项目内模块导入
-from src.core.utils.logger.crash_bundle import build_safe_config_summary, sanitize_text_for_export
-from src.core.utils.logger.log_enum import LogLevel, LogSource, LogType
-from src.core.utils.logger.log_func import logger
