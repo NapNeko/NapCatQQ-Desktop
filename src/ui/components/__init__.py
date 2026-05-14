8e80cd13f5a4334c8066ee39e5b212779c43a916
Qiao <SugarQiao@outlook.com>
2026-05-15 00:34:09 +0800

    feat(component): 导出 SettingSubtitle 和 SubtleSearchEdit 组件

--- src/ui/components/__init__.py
+++ src/ui/components/__init__.py
@@ -1,3 +1,5 @@
 # -*- coding: utf-8 -*-
 from .drop_folder_widget import DropFolderWidget
+from .setting_subtitle import SettingSubtitle
 from .skeleton_widget import SkeletonShape, SkeletonWidget
+from .subtle_search_edit import SubtleSearchEdit
